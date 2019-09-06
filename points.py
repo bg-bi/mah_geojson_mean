0a352fe61d833c8be2b61153b687e3fa185f19f0
Brandon <bgigous@boulderimaging.com>
2019-09-06 07:16:08 -0600

    code and more

---
+++ points.py
@@ -0,0 +1,10 @@
+import random
+
+with open("points.json", 'w') as pts_file:
+    pts_file.write('{"type": "MultiPoint", "coordinates": [\n')
+    num_pts = 1000
+    for i in range(num_pts):
+        pts_file.write(f'[{random.uniform(20,80)}, {random.uniform(20,80)}]')
+        if not i == num_pts - 1:
+            pts_file.write(',\n')
+    pts_file.write("\n]}")
